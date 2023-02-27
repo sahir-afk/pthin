35dbe6b99aaec3a8e150171b633cfb648664ad35
SahirP <83929092+sahir-afk@users.noreply.github.com>
2023-02-27 16:18:52 -0600

    Data Structures

--- tutorial/dataStructs/dataStructs.py
+++ tutorial/dataStructs/dataStructs.py
@@ -1,4 +1,6 @@
-class Stacks:
+
+# LIFO
+class Stack:
     def __init__(self):
         self.data = []
 
@@ -9,11 +11,45 @@ class Stacks:
         return self.data == []
     
     def pop(self):
-        self.data.pop()
+        return self.data.pop()
     
     def peek(self):
-        last = len(self.data - 1)
+        last = len(self.data) - 1
         return self.data[last]
     
     def size(self):
         return len(self.data)
+    
+stack = Stack()
+for letter in "Uzbekistan":
+    stack.push(letter)
+
+reverse = ""
+
+for i in range(len(stack.data)):
+    reverse += stack.pop()
+
+print(reverse)
+
+#FIFO
+class queue:
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return self.items == []
+    
+    def enqueue(self, val):
+        self.items.insert(0, val)
+    
+    def dequeue(self):
+        return self.items.pop()
+    
+    def size(self):
+        return len(self.items)
+    
+q = queue()
+for i in range(0, 7):
+    q.enqueue(i)
+
+print(q.dequeue())
