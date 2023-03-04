21214c43ed1bb602290e3b8681b26a57856f286b
SahirP <83929092+sahir-afk@users.noreply.github.com>
2023-03-04 13:21:16 -0600

    tutorials

--- tutorial/regex/reg.py
+++ tutorial/regex/reg.py
@@ -1,4 +1,6 @@
 import re
-phoneNumRegex = re.compile(r'Chloricious acidic diamenophene')
-num =  phoneNumRegex.search(" Yes, I seem ot have discovered a copious amount of Chloricious acidic diamenophene in the solution, it can only mean one thing: the chunin exams have been compromised!")
-print("FOUND:", num.group())
+phoneNumRegex = re.compile(r'(\d\d\d)-(\d\d\d-\d\d\d\d)')
+num =  phoneNumRegex.search(" Yes, I seem ot have discovered a copious amount of 651-432-0382 in the solution, it can only mean one thing: the chunin exams have been compromised!")
+print("FOUND:", num.groups())
+area_code, main_num = num.groups()
+print(area_code)
